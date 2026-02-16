af68837cf1c0cf8a3112a20b5492948b8254128c
9xjubayerofc <uruchimaru74@gmail.com>
2026-02-16 12:45:46 +0600

    Add files via upload

--- app.py
+++ app.py
@@ -242,8 +242,6 @@ import os
 
 if __name__ == '__main__':
     # Render
-    port = int(os.environ.get('PORT', 10000))
-    
-    # debug=True
-    socketio.run(app, host='0.0.0.0', port=port, debug=False)
+    port = int(os.environ.get("PORT", 10000))
+    socketio.run(app, host='0.0.0.0', port=port)
 
